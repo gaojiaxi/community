1066e2db82a50b8f1aba61e63326efac0d9e8795
gaojiaxi <gaojiaxi2015@gmail.com>
2020-12-21 16:56:02 +0800

    add exceptionhandler

--- src/main/java/com/nowcoder/community/controller/HomeController.java
+++ src/main/java/com/nowcoder/community/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -43,5 +44,10 @@ public class HomeController {
         }
         model.addAttribute("discussPosts", discussPosts);
         return "/index";
+    }
+
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage() {
+        return "/error/500/";
     }
 }
