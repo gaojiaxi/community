f081e028b73eef545d8e98d6169fe96e414af695
gaojiaxi <gaojiaxi2015@gmail.com>
2020-12-22 14:44:22 +0800

    add spring redis configuration, add Redis test

--- src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
+++ src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
@@ -35,6 +35,6 @@ public class ServiceLogAspect {
         String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
 
         logger.info(String.format("用户[%s],在[%s],访问了[%s]", ip, now, target));
-        
+
     }
 }
