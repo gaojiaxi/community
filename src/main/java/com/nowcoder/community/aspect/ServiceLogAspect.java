d9ca6b6b0d7e05a833953b5a4aa2321f68e9e6e3
gaojiaxi <gaojiaxi2015@gmail.com>
2020-12-25 12:59:01 +0800

    update LikeController FollowController CommentController, fixed bug in message-mapper, update discuss-detial.html and discuss.js

--- src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
+++ src/main/java/com/nowcoder/community/aspect/ServiceLogAspect.java
@@ -29,6 +29,9 @@ public class ServiceLogAspect {
     public void before(JoinPoint joinPoint){
         // 用户(ip){1.2.3.4}, 在[xxx时间], 访问了[com.nowcoder.community.service.xxx()]
         ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) {
+            return;
+        }
         HttpServletRequest request = attributes.getRequest();
         String ip = request.getRemoteHost();
         String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
--- src/main/java/com/nowcoder/community/dao/CommentMapper.java
+++ src/main/java/com/nowcoder/community/dao/CommentMapper.java
@@ -14,6 +14,8 @@ public interface CommentMapper {
 
     int insertComment(Comment comment);
 
+    Comment selectCommentById(int id);
+
 
 
 }
