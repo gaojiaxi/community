95194bc92893df68f4bd0ca22f086f2abd46a78b
gaojiaxi <gaojiaxi2015@gmail.com>
2020-12-27 12:34:50 +0800

    add system notice list

--- src/main/java/com/nowcoder/community/service/MessageService.java
+++ src/main/java/com/nowcoder/community/service/MessageService.java
@@ -60,4 +60,8 @@ public class MessageService {
         return messageMapper.selectLetterUnreadCount(userId, topic);
     }
 
+    public List<Message> findNotices(int userId, String topic, int offset, int limit) {
+        return messageMapper.selectNotices(userId, topic, offset, limit);
+    }
+
 }
