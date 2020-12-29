733cc06c529882aa2e2986269510852f63b764fd
gaojiaxi <gaojiaxi2015@gmail.com>
2020-12-29 12:02:13 +0800

    add ElasticsearchTests, create new branch deves

---
+++ src/test/java/com/nowcoder/community/ElasticsearchTests.java
@@ -0,0 +1,36 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class ElasticsearchTests {
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private DiscussPostRepository discussPostRepository;
+
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+    @Test
+    public void testInsert() {
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(241));
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(242));
+        discussPostRepository.save(discussPostMapper.selectDiscussPostById(243));
+    }
+
+
+
+
+}
