c71108dc2e0592d25b73565dc4bf272c22fcd5b2
gaojiaxi <gaojiaxi2015@gmail.com>
2020-12-29 13:56:44 +0800

    add ElasticSearchService, allow users to use API to save, delete and search disscuss post from elastic search server

---
+++ src/main/java/com/nowcoder/community/service/ElasticsearchService.java
@@ -0,0 +1,119 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ElasticsearchService {
+
+    @Autowired
+    private DiscussPostRepository discussPostRepository;
+
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
+
+    // 向Elasticsearch 服务器提交新产生的帖子
+    public void saveDiscussPost(DiscussPost post) {
+        discussPostRepository.save(post);
+    }
+
+    // 从Elasticsearch 服务器删除指定的帖子
+    public void deleteDiscussPost(int id) {
+        discussPostRepository.deleteById(id);
+    }
+
+    // 从Elasticssearch 服务器搜索帖子
+    public Page<DiscussPost> searchDiscussPost(String keyword, int currentPage, int limit) {
+        // 构造搜索条件和搜索设置
+        // 关键词 + 排序方式 + 分页方式 + 高亮显示
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                // 搜索条件 标题 + 正文
+                .withQuery(QueryBuilders.multiMatchQuery(keyword, "title", "content"))
+                // 排序方式
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                // 分页方式
+                .withPageable(PageRequest.of(currentPage, limit))
+                // 高亮显示
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+
+        return elasticsearchTemplate.queryForPage(searchQuery, DiscussPost.class, new SearchResultMapper() {
+            @Override
+            public <T> AggregatedPage<T> mapResults(SearchResponse response, Class<T> clazz, Pageable pageable) {
+                SearchHits hits = response.getHits();
+                if (hits.getTotalHits() <= 0) {
+                    return null;
+                }
+                List<DiscussPost> list = new ArrayList<>();
+                for (SearchHit hit : hits) {
+                    DiscussPost post = new DiscussPost();
+                    String id = hit.getSourceAsMap().get("id").toString();
+                    post.setId(Integer.valueOf(id));
+
+                    String userId = hit.getSourceAsMap().get("userId").toString();
+                    post.setUserId(Integer.valueOf(userId));
+
+                    String title = hit.getSourceAsMap().get("title").toString();
+                    post.setTitle(title);
+
+                    String content = hit.getSourceAsMap().get("content").toString();
+                    post.setContent(content);
+
+                    String status = hit.getSourceAsMap().get("status").toString();
+                    post.setStatus(Integer.valueOf(status));
+
+                    String createTime = hit.getSourceAsMap().get("createTime").toString();
+                    post.setCreateTime(new Date(Long.valueOf(createTime)));
+
+                    String commentCount = hit.getSourceAsMap().get("commentCount").toString();
+                    post.setCommentCount(Integer.valueOf(commentCount));
+
+                    // 处理高亮显示的结果
+                    HighlightField titleField = hit.getHighlightFields().get("title");
+                    if (titleField != null) {
+                        // 返回的是数组，因为title中可能有多个匹配，取第一段
+                        post.setTitle(titleField.getFragments()[0].toString());
+                    }
+
+                    HighlightField contentField = hit.getHighlightFields().get("content");
+                    if (contentField != null) {
+                        // 返回的是数组，因为content中可能有多个匹配，取第一段
+                        post.setContent(contentField.getFragments()[0].toString());
+                    }
+                    list.add(post);
+                }
+
+                return new AggregatedPageImpl(list, pageable,
+                        hits.getTotalHits(), response.getAggregations(), response.getScrollId(), hits.getMaxScore());
+            }
+        });
+    }
+
+}
